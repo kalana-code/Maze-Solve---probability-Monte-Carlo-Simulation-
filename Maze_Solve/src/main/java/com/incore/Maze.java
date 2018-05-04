5aac924540c78ac4196329b93b472f32ea6089d0
Kalana Dhananjaya <Emny.incore@gmail.com>
2018-05-04 06:06:25 +0530

    Three thread added for reduce time complexcity

--- Maze_Solve/src/main/java/com/incore/Maze.java
+++ Maze_Solve/src/main/java/com/incore/Maze.java
@@ -1,15 +1,23 @@
-package com.incore;
+
 
 import java.util.Random;
 
-class Maze {
+class Maze extends Thread {
+    //this hold Number of path have
+    private static int solving_count;
+    // this hold number of test have done
+    private static int Number_of_test;
+
 
     private int [][] maze;
+    private int no_of_iteration;
 
-    public Maze() {
-
-        MazeGenarator(30);
+    public Maze(int size_of_maze, int no_of_iteration) {
+        this.no_of_iteration= no_of_iteration;
+        Number_of_test+=no_of_iteration;
     }
+
+
 
     public void MazeGenarator(int size){
         Random ran = new Random();
@@ -17,14 +25,16 @@ class Maze {
 
         for (int i=0;i<size;i++){
             for (int j =0;j<size;j++){
-                tmp[i][j]=ran.nextInt(999999999)%2;
+                tmp[i][j]=ran.nextInt(2);
             }
         }
         tmp[0][0]=0;
         tmp[size-1][size-1]=0;
-        maze= tmp;
+        this.maze= tmp;
 
     }
+
+
 
     public void show() {
         for(int i=0; i < maze.length; i++) {
@@ -37,7 +47,7 @@ class Maze {
 
 
     public boolean findPath(int sx, int sy, int ex, int ey) {
-        maze[sx][sy]=3;
+        maze[sx][sy]=1;
         if(sx==ex && sy==ey)
              return  true;
         // when some point check it  check their moving possibilities for 4 direction
@@ -53,8 +63,9 @@ class Maze {
         if((sy-1>0) && (maze[sx][sy-1])==0 &&  findPath(sx,sy-1,ex,ey))
              return true;
 
-        maze[sx][sy]=0;
+        //maze[sx][sy]=0;
         return  false;
+
 
     }
 
@@ -62,19 +73,47 @@ class Maze {
     }
 
 
-    public static void main(String [] args) {
+    public void run(){
+        int i=0;
+        //System.out.println("run");
+        while (i<no_of_iteration) {
+
+            MazeGenarator(20);
+
+            if (findPath(0, 0, 19, 19)) {
+
+                synchronized(this) {
+                    solving_count++;
+                }
+
+            }
+            //System.out.println(i);
+            i++;
+
+        }
+        //System.out.println("erun");
+    }
 
 
-            Maze m = new Maze();
-            m.show();
+    public static void main(String [] args) throws InterruptedException {
+        int size_of_maze= 20;
+        int test_case = 10000000;
 
-            if (m.findPath(0, 0, 30, 30)) {
-                System.out.println("There is a path");
-                m.showPath(0, 0, 30, 30);
-                m.show();
-            } else
-                //m.show();
-                System.out.println("There is no path");
+        Maze m1= new Maze(size_of_maze,test_case);
+        Maze m2= new Maze(size_of_maze,test_case);
+        Maze m3= new Maze(size_of_maze,test_case);
 
+        m1.start();
+        m2.start();
+        m3.start();
+
+        m3.join();
+        m2.join();
+        m1.join();
+
+
+        System.out.println(solving_count);
+
+        System.out.println(solving_count/(double)Number_of_test);
     }
 }
